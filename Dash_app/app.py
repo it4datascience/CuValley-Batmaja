c393a0a2c0f92e9c326e1c2401264804e3805f89
Jakub Zawistowski <it4datascience@gmail.com>
2023-01-22 23:01:44 +0100

    change dash layout

--- Dash_app/app.py
+++ Dash_app/app.py
@@ -1,25 +1,40 @@
 import dash
 from dash import dcc
 from dash import html
-
 app = dash.Dash(__name__, use_pages=True)
 server = app.server
 app.config.suppress_callback_exceptions=True
 
 
 app.layout = html.Div([
+html.Div([
+    html.Div(
+        html.Img(src='assets/logo_BATMAJA.png', className='center-image', style={
+                'height': '20%',
+                'width': '20%',
+            }),
+        className='center-div'
+    ),
+    html.A(
+    html.Div(
+        html.Img(src='assets/logo_CUVALLEY.jpg', className='right-top-image'),
+        className='right-top-div'
+    ), href='https://cuvalley.com/stworzenie-systemu-automatycznej-estymacji-poziomu-wody-w-rzece/')
+], style={"display":"flex"}),
+    html.H3('RIVER ESTIMATOR',
+            style={'text-align': 'center', 'color': '#035891'}),
+
+
     html.Div(
         [
             html.Div([
                 dcc.Link(
-                    html.Button(f"{page['name']}"), href=page["path"]
+                    html.Button(f"{page['name']}", style={"margin-right":"1%", "margin-left":"1%"}), href=page["path"],
                 )
                 for page in dash.page_registry.values()]
             )
-        ]
+        ], style={'textAlign': 'center'}
     ),
-    html.H3('STWORZENIE SYSTEMU AUTOMATYCZNEJ ESTYMACJI POZIOMU WODY W RZECE',
-            style={'text-align':'center', 'color':'#035891'}),
 
 	dash.page_container
 ])
--- Dash_app/pages/Interpretacja_modelu.py
+++ Dash_app/pages/Interpretacja_modelu.py
@@ -1,9 +1,23 @@
 import dash
 from dash import html, dcc,callback, Input, Output
+from joblib import load
+import pandas as pd
+import dalex as dx
+from src.plots import DashExplainers
 dash.register_page(__name__, order=2)
 
+
+with open('../Dash_app/assets/fitted_model.pkl', 'rb') as file:
+    model = load(file)
+X_test = pd.read_csv('../Dash_app/assets/X_test.csv')
+y_test = pd.read_csv('../Dash_app/assets/y_test.csv')
+
+de = DashExplainers(model, X_test, y_test)
+importance = de.plot_importance()
+
+
+
+
 layout = html.Div(
-    children=[
-        html.H6('dddddd')
-    ]
+        dcc.Graph(id="graph-3", figure=importance)
 )
