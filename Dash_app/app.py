5f7fe8930424f18cdd6c29c095a4b6ef52073ca7
Jakub Zawistowski <it4datascience@gmail.com>
2023-01-27 00:12:59 +0100

    Change Dash Layout

--- Dash_app/app.py
+++ Dash_app/app.py
@@ -10,26 +10,30 @@ app.layout = html.Div([
 html.Div([
     html.Div(
         html.Img(src='assets/logo_BATMAJA.png', className='center-image', style={
-                'height': '20%',
                 'width': '20%',
             }),
-        className='center-div'
+        className='left-div'
     ),
     html.A(
     html.Div(
-        html.Img(src='assets/logo_CUVALLEY.jpg', className='right-top-image'),
+        html.Img(src='assets/logo_cuvalley.png', className='right-top-image', style={
+                'width': '70%',
+        }),
         className='right-top-div'
     ), href='https://cuvalley.com/stworzenie-systemu-automatycznej-estymacji-poziomu-wody-w-rzece/')
 ], style={"display":"flex"}),
-    html.H3('RIVER ESTIMATOR',
-            style={'text-align': 'center', 'color': '#035891'}),
+    html.Br(),
+    html.H2([html.Span('R',style={'color':'#fcb040', 'font-size':'4rem'}),'iver ',
+             html.Span('E',style={'color':'#fcb040','font-size':'4rem'}),'stimator'],
+            style={'text-align': 'center', 'color': 'white', 'font-family':'Jua, sans-serif','font-weight': 'bold'}),
 
 
     html.Div(
         [
             html.Div([
                 dcc.Link(
-                    html.Button(f"{page['name']}", style={"margin-right":"1%", "margin-left":"1%"}), href=page["path"],
+                    html.Button(f"{page['name']}", style={"margin-right":"1%", "margin-left":"1%", 'color':'white',
+        'border-width':'2.5px'}), href=page["path"],
                 )
                 for page in dash.page_registry.values()]
             )
